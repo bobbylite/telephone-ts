c235487f5c7a8442651e9c391f90d6d97fc3a6e9
Robert Luisi <bobby.luisi@gmail.com>
2019-02-27 16:58:22 -0500

    First Commit.

---
+++ build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Telephone = /** @class */ (function () {
+    function Telephone() {
+    }
+    Telephone.Test = function () {
+        console.log("Test");
+    };
+    return Telephone;
+}());
+exports.Telephone = Telephone;
+Telephone.Test();
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export class Telephone { 
+
+    public static Test() : void {
+        console.log("Test");
+    }
+}
+
+Telephone.Test();
