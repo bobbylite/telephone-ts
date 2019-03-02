23c288166934afd59b92a9aaceb2aab87eb5b585
Robert Luisi <bobby.luisi@gmail.com>
2019-03-02 02:35:55 -0500

    Added better code to show events being triggered in sync time.

--- src/test/test.ts
+++ src/test/test.ts
@@ -4,11 +4,18 @@ import { NotHelloHandler, HelloHandler } from "./testhandlers";
 import { HelloEvent, NotHelloEvent } from "./testevents";
 import { IHelloEvent, INotHelloEvent } from "./testinterfaces";
 
-var telephonets = new Telephonets();
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-telephonets.CreateQuietListeningWire<INotHelloEvent>("INotHelloEvent", NotHelloHandler);
-telephonets.CreateQuietListeningWire<IHelloEvent>("IHelloEvent", HelloHandler);
+async function Test() {
+    var telephonets = new Telephonets();
+
+    telephonets.CreateQuietListeningWire<INotHelloEvent>("INotHelloEvent", NotHelloHandler);
+    telephonets.CreateQuietListeningWire<IHelloEvent>("IHelloEvent", HelloHandler);
 
 
-telephonets.ShoutOnWire<IHelloEvent>("IHelloEvent", new HelloEvent);
-telephonets.ShoutOnWire<INotHelloEvent>("INotHelloEvent", new NotHelloEvent);
+    telephonets.ShoutOnWire<IHelloEvent>("IHelloEvent", new HelloEvent);
+    await sleep(1000);
+    telephonets.ShoutOnWire<INotHelloEvent>("INotHelloEvent", new NotHelloEvent);
+}
+
+Test();
